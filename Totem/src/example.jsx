b9acda7148ec414f2b24fa374eb3630ce1bfd571
benja3086 <benjaminquinteros785@gmail.com>
2026-06-03 15:03:21 -0300

    Primer commit

---
+++ Totem/src/example.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./example.css";
+function example() {
+  return (
+    <div className="example">
+      <h2>example</h2>
+      <h1>example</h1>
+      <h5>example</h5>    
+      <button>example</button>
+      <img src="" alt="" />
+    </div>
+  );  
+  
+}
+
+export default example;
